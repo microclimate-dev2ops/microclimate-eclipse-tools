54e72785e7891cddde859e090e0abf899ecfa4be
Tim Etchells <timetchells@ibm.com>
2018-09-10 16:56:47 -0400

    Add copyrights to everything but build files

--- dev/com.ibm.microclimate.ui/src/com/ibm/microclimate/ui/internal/server/actions/LogActionProvider.java
+++ dev/com.ibm.microclimate.ui/src/com/ibm/microclimate/ui/internal/server/actions/LogActionProvider.java
@@ -1,4 +1,13 @@
 /*******************************************************************************
+ * IBM Confidential
+ * OCO Source Materials
+ * (C) Copyright IBM Corp. 2018 All Rights Reserved.
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets, irrespective of what has
+ * been deposited with the U.S. Copyright Office.
+ *******************************************************************************/
+
+/*******************************************************************************
  * Copyright (c) 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
