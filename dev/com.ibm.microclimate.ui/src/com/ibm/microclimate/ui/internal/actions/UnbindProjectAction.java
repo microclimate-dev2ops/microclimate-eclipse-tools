e5c3b7203ef5549c9d28654b99dd92a5619641ce
Erin Harris <eharris@ca.ibm.com>
2019-05-13 16:49:51 -0400

    Add unbind action

---
+++ dev/com.ibm.microclimate.ui/src/com/ibm/microclimate/ui/internal/actions/UnbindProjectAction.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *	 IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.microclimate.ui.internal.actions;
+
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.SelectionProviderAction;
+
+import com.ibm.microclimate.core.internal.MCEclipseApplication;
+import com.ibm.microclimate.core.internal.MCLogger;
+import com.ibm.microclimate.core.internal.MCUtil;
+
+/**
+ * Action for unbinding a Codewind project.
+ */
+public class UnbindProjectAction extends SelectionProviderAction {
+	
+	MCEclipseApplication app;
+	
+	public UnbindProjectAction(ISelectionProvider selectionProvider) {
+		super(selectionProvider, "Remove");
+		selectionChanged(getStructuredSelection());
+		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ETOOL_DELETE));
+	}
+
+	@Override
+	public void selectionChanged(IStructuredSelection sel) {
+		if (sel.size() == 1) {
+			Object obj = sel.getFirstElement();
+			if (obj instanceof MCEclipseApplication) {
+				app = (MCEclipseApplication) obj;
+				setEnabled(true);
+				return;
+			}
+		}
+		setEnabled(false);
+	}
+
+	@Override
+	public void run() {
+		if (app == null) {
+			// should not be possible
+			MCLogger.logError("UnbindProjectAction ran but no application was selected");
+			return;
+		}
+		
+		try {
+			app.mcConnection.requestProjectDelete(app.projectID);
+		} catch (Exception e) {
+			MCLogger.logError("Error requesting application remove: " + app.name, e); //$NON-NLS-1$
+			MCUtil.openDialog(true, "An error occurred trying to remove the " + app.name + " application from Codewind.", e.getMessage());
+			return;
+		}
+	}
+}
